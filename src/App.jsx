801ae8f69adaa7b9fa47f250aacda64995754ee6
Deiby24 <deiby.8@hotmail.es>
2025-04-08 10:06:56 -0500

    improve index

--- src/App.jsx
+++ src/App.jsx
@@ -25,17 +25,22 @@ const App = () => {
 
   return (
     <>
-    <Helmet>
-      <title>LCS Libertas Consulting Services</title>
-      <meta name="description" content="Comprehensive Libertas solutions and personalized advice for all your immigration needs. Get your free consultation today." />
-      {/* SEO Open Graph */}
-      <meta property="og:title" content="Immigration Law Experts | LCS Libertas Consulting Services" />
-      <meta property="og:description" content="Trusted and personalized Libertas assistance for immigration matters. Citizenship, residence, visas and more." />
-      <meta property="og:url" content="https://libertasconsultingservice.com" />
-      <meta property="og:type" content="website" />
-      {/* Language */}
-      <html lang="en" />
-    </Helmet>
+<Helmet>
+ 
+  <title>Libertas Consulting Service | Experts in immigration consulting</title>
+
+  <meta name="description" content="Libertas offers personalized immigration consulting for residency, citizenship, visas, and more. Get your free consultation today." />
+
+
+  <meta property="og:title" content="Immigration Law Experts | Libertas Consulting Services" />
+  <meta property="og:description" content="Professional and trusted immigration advice. Get your free consultation with our experts today." />
+  <meta property="og:url" content="https://libertasconsultingservice.com" />
+  <meta property="og:type" content="website" />
+  <meta property="og:image" content="https://libertasconsultingservice.com/libertaswhite.png" />
+  <meta property="og:image:alt" content="Libertas Consulting Services Logo" />
+
+  <html lang="en" />
+</Helmet>
     <Router basename={process.env.PUBLIC_URL || ''}>
       <ScrollToTop/>
       <Header />
